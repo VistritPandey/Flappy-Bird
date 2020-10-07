7476322f53c46e87d987668344258ac5b2a5a33d
vistritpandey <vistritpandey@gmail.com>
2020-10-07 16:18:33 +0530

    f

--- app.js
+++ app.js
@@ -2,28 +2,35 @@ document.addEventListener('DOMContentLoaded' , () => {
     const bird = document.querySelector('.bird')
     const gameDisplay = document.querySelector('.game-container')
     const ground = document.querySelector('.ground-moving')
+
     let birdLeft = 220
     let birdBottom = 100
     let gravity = 3
     let isGameOver = false
     let gap = 430
+
+
     function startGame() {
         birdBottom -= gravity
         bird.style.bottom = birdBottom + 'px'
         bird.style.left = birdLeft + 'px'
     }
     let gameTimerId = setInterval(startGame, 20)
+
     function control(e) {
         if (e.keyCode === 32) {
             jump()
         }
     }
+
     function jump() {
         if (birdBottom < 500) birdBottom += 50
         bird.style.bottom = birdBottom + 'px'
         console.log(birdBottom)
     }
-    document.addEventListener('keyup', control)
+    document.addEventListener('keyup', jump)
+
+
     function generateObstacle() {
         let obstacleLeft = 500
         let randomHeight = Math.random() * 60
@@ -40,10 +47,12 @@ document.addEventListener('DOMContentLoaded' , () => {
         topObstacle.style.left = obstacleLeft + 'px'
         obstacle.style.bottom = obstacleBottom + 'px'
         topObstacle.style.bottom = obstacleBottom + gap + 'px'
+
         function moveObstacle() {
             obstacleLeft -=2
             obstacle.style.left = obstacleLeft + 'px'
             topObstacle.style.left = obstacleLeft + 'px'
+
             if (obstacleLeft === -60) {
                 clearInterval(timerId)
                 gameDisplay.removeChild(obstacle)
@@ -60,8 +69,11 @@ document.addEventListener('DOMContentLoaded' , () => {
         }
         let timerId = setInterval(moveObstacle, 20) 
         if (!isGameOver) setTimeout(generateObstacle, 3000)
+
     }
     generateObstacle()
+
+
     function gameOver() {
         clearInterval(gameTimerId)
         console.log('game over')
@@ -70,4 +82,6 @@ document.addEventListener('DOMContentLoaded' , () => {
         ground.classList.add('ground')
         ground.classList.remove('ground-moving')
     }
+
+
 })
