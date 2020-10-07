ee1386ddd857aad4d1edda607396d1c5e510bac3
vistritpandey <vistritpandey@gmail.com>
2020-10-07 15:59:10 +0530

    f

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('DOMContentLoaded' , () => {
     const bird = document.querySelector('.bird')
-})
+    const gameDisplay = document.querySelector('.game-container')
+    const ground = document.querySelector('.ground-moving')
+
+    let birdLeft = 220
+    let birdBottom = 100
+    let gravity = 3
+    let isGameOver = false
+    let gap = 430
+
+
+    function startGame() {
+        birdBottom -= gravity
+        bird.style.bottom = birdBottom + 'px'
+        bird.style.left = birdLeft + 'px'
